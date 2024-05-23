b1b387303ec75786300579e0956d380bcbe3197e
LucaMuntele16 <119846365+LucaMuntele16@users.noreply.github.com>
2024-05-24 00:17:13 +0300

    p

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const buttonStart = document.querySelector(".start");
+const buttonStop = document.querySelector(".stop");
+buttonStop.disabled=true;
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+// var fct = setInterval(getRandomHexColor,1000);
+
+// const handleClick = () => {
+//   document.body.style.background = setInterval(() => {
+//     getRandomHexColor
+//   }, 1000);
+// };
+
+
+buttonStart.addEventListener("click", () =>{
+ timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor()
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+  }, 1000);
+});
+
+buttonStop.addEventListener("click", () =>{
+  clearInterval(timerId);
+  buttonStart.disabled = false;
+    buttonStop.disabled = true;
+})
 
