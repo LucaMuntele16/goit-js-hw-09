3150a27ffcf9ea102aaed3cbaa293de6f1a69278
LucaMuntele16 <119846365+LucaMuntele16@users.noreply.github.com>
2024-11-04 22:11:06 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,26 +1,8 @@
-document.querySelector('.form').addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const delay = Number(event.target.delay.value);
-  const step = Number(event.target.step.value);
-  const amount = Number(event.target.amount.value);
-
-  for (let i = 1; i <= amount; i++) {
-    const currentDelay = delay + step * (i - 1);
-    createPromise(i, currentDelay)
-      .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-  }
-});
 
 function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
@@ -29,3 +11,28 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
+
+
+document.querySelector('.form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delayInput = parseInt(event.target.delay.value);
+  const stepInput = parseInt(event.target.step.value);
+  const amountInput = parseInt(event.target.amount.value);
+
+  let delay = delayInput;
+
+
+  for (let i = 1; i <= amountInput; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    
+  
+    delay += stepInput;
+  }
+});
